380465c86aa90f76d5ee3db10c567e1221bc865f
Philip Johnson <p.zhao.johnson@gmail.com>
2024-09-02 22:34:56 -0400

    The deepCopy() function can now also copy individual position pairs

--- src/deepCopy.test.js
+++ src/deepCopy.test.js
@@ -1,27 +1,37 @@
 import { deepCopy } from './deepCopy';
 
-describe('deepCopy function', () => {
-  it('creates a new array', () => {
-    let pairs = [[1, 2], [3, 4], [5, 6]];
+describe('`deepCopy()` function', () => {
+  test('copying a position pair', () => {
+    let pair = [57, 81];
+    let pairCopy = deepCopy(pair);
 
-    expect(deepCopy(pairs)).not.toBe(pairs);
+    expect(pairCopy).toStrictEqual([57, 81]);
+    expect(pairCopy).not.toBe(pair);
   });
 
-  it('creates new position pairs', () => {
-    let pairs = [[9, 1], [2, 8], [3, 7], [4, 6]];
+  describe('copying arrays of position pairs', () => {
+    it('creates a new array', () => {
+      let pairs = [[1, 2], [3, 4], [5, 6]];
 
-    deepCopy(pairs).forEach(pr => {
-      expect(pairs.includes(pr)).toBeFalsy();
+      expect(deepCopy(pairs)).not.toBe(pairs);
     });
-  });
 
-  it('maintains values', () => {
-    let pairs = [[112, 5], [8, 21], [30, 52], [1, 9], [2, 88]];
+    it('creates new position pair tuples', () => {
+      let pairs = [[9, 1], [2, 8], [3, 7], [4, 6]];
 
-    expect(deepCopy(pairs)).toStrictEqual([[112, 5], [8, 21], [30, 52], [1, 9], [2, 88]]);
-  });
+      deepCopy(pairs).forEach(pr => {
+        expect(pairs.includes(pr)).toBeFalsy();
+      });
+    });
 
-  test('an empty array of position pairs', () => {
-    expect(deepCopy([])).toStrictEqual([]);
+    it('copies the paired positions', () => {
+      let pairs = [[112, 5], [8, 21], [30, 52], [1, 9], [2, 88]];
+
+      expect(deepCopy(pairs)).toStrictEqual([[112, 5], [8, 21], [30, 52], [1, 9], [2, 88]]);
+    });
+
+    test('an empty array of position pairs', () => {
+      expect(deepCopy([])).toStrictEqual([]);
+    });
   });
 });
--- src/deepCopy.ts
+++ src/deepCopy.ts
@@ -1,8 +1,19 @@
 import type { PositionPair } from './PositionPair';
 
+import { isNumbersArray } from '@rnacanvas/value-check';
+
+export function deepCopy(pair: PositionPair): PositionPair;
+
+export function deepCopy(pairs: PositionPair[]): PositionPair[];
+
 /**
- * Returns a deep copy of the array of position pairs.
+ * Returns a deep copy of a position pair or an array of position pairs.
  */
-export function deepCopy(pairs: PositionPair[]): PositionPair[] {
-  return pairs.map(pr => [...pr]);
+export function deepCopy(pairs: PositionPair | PositionPair[]): PositionPair | PositionPair[] {
+  if (isNumbersArray(pairs)) {
+    return [...pairs];
+  } else {
+    let pairsCopy: PositionPair[] = pairs.map(pr => [...pr]);
+    return pairsCopy;
+  }
 }
