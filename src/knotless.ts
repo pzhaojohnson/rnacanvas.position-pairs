10e301ced50e3d20840f9fd14eb96c87fdbc0b1b
Philip Johnson <p.zhao.johnson@gmail.com>
2025-07-13 15:54:01 -0400

    Make knotless() and radializable() functions faster

--- src/knotless.ts
+++ src/knotless.ts
@@ -2,7 +2,7 @@ import type { PositionPair } from './PositionPair';
 
 import { deepCopy } from './deepCopy';
 
-import { areKnotless } from './areKnotless';
+import { min, max } from '@rnacanvas/math';
 
 /**
  * Returns a deep copy of the provided pairs array
@@ -20,9 +20,20 @@ export function knotless(pairs: PositionPair[]): PositionPair[] {
   // sort by the distance spanned by each pair
   pairsCopy.sort((pr1, pr2) => Math.abs(pr1[0] - pr1[1]) - Math.abs(pr2[0] - pr2[1]));
 
+  let spanned: (boolean | undefined)[] = [];
+
   pairsCopy.forEach(pr => {
-    if (areKnotless([...noPseudoknots, pr])) {
+    let indices = [pr[0] - 1, pr[1] - 1];
+
+    let minIndex = min(indices);
+    let maxIndex = max(indices);
+
+    if (!spanned[minIndex] && !spanned[maxIndex]) {
       noPseudoknots.push(pr);
+
+      for (let i = minIndex; i <= maxIndex; i++) {
+        spanned[i] = true;
+      }
     }
   });
 
